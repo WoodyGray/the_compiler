d8db2c3e58df857ce11854639b6cf28a1ff0ff71
WoodyGray <115577474+WoodyGray@users.noreply.github.com>
2024-03-12 11:27:15 +0300

    commit

--- test-script/test-script/src/main/java/antlr/App.java
+++ test-script/test-script/src/main/java/antlr/App.java
@@ -1,13 +1,20 @@
 package antlr;
 
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.Lexer;
+import org.antlr.v4.runtime.UnbufferedCharStream;
+import org.antlr.v4.runtime.misc.Interval;
+
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException {
+        Lexer lexer = new TestScriptLexer(new ANTLRFileStream("D:\\study\\github\\the_compiler\\test-program.txt"));
     }
 }
